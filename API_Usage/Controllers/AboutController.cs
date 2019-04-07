f16e75b898eaca85cb5098933cc06ee94854d1ae
Vidya Sagar Gunnala <viddi89s@gmail.com>
2019-04-07 12:08:44 -0400

    Updated about page and corrected few points in controllers

---
+++ API_Usage/Controllers/AboutController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Usage.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
